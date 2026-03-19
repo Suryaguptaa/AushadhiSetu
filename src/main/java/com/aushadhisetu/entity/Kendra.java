6d108d888f0050aebfabf10f6ee1795368e12185
Surya Gupta <181776742+Suryaguptaa@users.noreply.github.com>
2026-03-19 23:02:56 +0530

    Initial commit

---
+++ src/main/java/com/aushadhisetu/entity/Kendra.java
@@ -0,0 +1,52 @@
+package com.aushadhisetu.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+import java.time.LocalTime;
+
+@Entity
+@Data
+public class Kendra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    private String owner;
+    @Column(nullable = false)
+    private String address;
+    @Column(nullable = false)
+    private String city;
+
+    @Pattern(regexp = "\\d{10}", message = "Phone number must be 10 digits")
+    private String phoneNumber;
+
+    @Pattern(regexp = "\\d{6}", message = "Pincode must be exactly 6 digits")
+    @Column(length = 6, nullable = false)
+    private String pincode;
+
+    @Column(nullable = false)
+    private LocalTime openingTime;
+    @Column(nullable = false)
+    private LocalTime closingTime;
+
+    private boolean manuallyClosed;
+    public boolean isOpenNow() {
+        if (manuallyClosed) return false;
+
+        LocalTime now = LocalTime.now();
+        if (closingTime.isAfter(openingTime)) {
+            return now.isAfter(openingTime) && now.isBefore(closingTime);
+        } else {
+            return now.isAfter(openingTime) || now.isBefore(closingTime);
+        }
+    }
+
+
+
+
+}
